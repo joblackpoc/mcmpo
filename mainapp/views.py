b0473542f38164ac0d04e05ae734b8ede85c34be
joblackpoc <duopoc@gmail.com>
2024-09-02 17:04:56 +0700

    add template

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, DeleteView
+from . models import Post
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
