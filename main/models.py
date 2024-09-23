7e119a9a3228a5468e9a0c553e6307cbb644844d
MASTERPOP-N <duopoc@gmail.com>
2024-09-23 12:11:22 +0700

    add ckeditor 5

--- main/models.py
+++ main/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django_ckeditor_5.fields import CKEditor5Field
 # Create your models here.
 class Header(models.Model):
     header_title = models.CharField(max_length=40)
-    header_description = models.CharField(max_length=100)
+    header_description = CKEditor5Field(config_name='extends')
     header_image = models.ImageField(upload_to='header')
 
     def __str__(self):
--- mproject/urls.py
+++ mproject/urls.py
@@ -22,4 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('ckeditor5/', include('django_ckeditor_5.urls'), name='ckeditor5_uploadfile'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
